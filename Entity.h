34e5b05a5e90e328a85b63f4a3bb7c81e0c4e0c2
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-03-14 00:58:09 +0800

    Create Entity.h

---
+++ Entity.h
@@ -0,0 +1,37 @@
+// entity.h
+#pragma once
+#include <cstdint>
+#include <functional>
+
+namespace ecs 
+{
+  
+  struct Entity 
+  {
+      uint32_t index;
+      uint32_t generation;
+  
+      bool operator==(Entity other) const noexcept 
+      {
+          return index == other.index && generation == other.generation;
+      }
+
+      bool operator!=(Entity other) const noexcept { return !(*this == other); }
+  };
+
+  constexpr Entity ENTITY_NULL = { ~0u, 0u };
+
+} // namespace ecs
+
+
+// Specialize std::hash for use in unordered containers
+namespace std 
+{
+  template<> struct hash<ecs::Entity> 
+  {
+    size_t operator()(ecs::Entity e) const noexcept 
+    {
+        return (size_t(e.index) << 32) | e.generation;
+    }
+  };
+}
