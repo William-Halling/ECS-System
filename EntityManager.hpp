a8744b6a89745fcb25150b90cb50a1fc7731d802
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-06-27 22:51:13 +0800

    Update EntityManager.hpp

--- EntityManager.hpp
+++ EntityManager.hpp
@@ -4,15 +4,21 @@
 #include <cstdint>
 
 using Entity = std::uint32_t;
-
-class EntityManager
+namespace ecs 
 {
-private:
-  Entity nextEntity = 0;
-
-public: 
-  Entity createEntity()
+  class EntityManager
   {
-    return nextEntity++;
-  }
-};
+    private:
+      uint32_t next_index = 0;
+      std::vector<uint32_t> generations;
+      std::vector<uint32_t> free_indices;
+  
+    public:
+      EntityManager() = default;
+      ~EntityManager() = default;
+
+      Entity createEntity();
+      bool destroyEntity(Entity entity);
+      [[nodiscard]] bool isAlive(Entity entity) const noexcept;
+  };
+}
