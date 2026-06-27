0d1c8241d7eb2198185b5bccabaaf51ad4f53574
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-06-27 22:59:05 +0800

    Update component.h

--- component.h
+++ component.h
@@ -3,14 +3,13 @@
 #include <cstddef>
 #include <cstdint>
 #include <type_traits>
-#include <functional>
+#include <utility>
 
 namespace ecs 
 {
-  
   using ComponentTypeId = uint32_t;
   
-  // Unique ID generator for component types
+    // Unique ID generator for component types
   inline ComponentTypeId next_component_type_id() noexcept 
   {
       static ComponentTypeId counter = 0;
@@ -25,39 +24,43 @@ namespace ecs
       
       return id;
   }
-  
-  
-  // Type-erased component traits
+
+
   struct ComponentTraits 
   {
-      size_t size;
-      size_t alignment;
-      void (*move_construct)(void* dst, void* src);
-      void (*copy_construct)(void* dst, const void* src);
-      void (*destruct)(void* ptr);
+    size_t size;
+    size_t alignment;
+    void (*move_construct)(void* dst, void* src);
+    void (*copy_construct)(void* dst, const void* src);
+    void (*destruct)(void* ptr);
+    bool is_trivially_copyable;
   };
-  
-  
-  // Helper to create traits for a concrete component type
+
+
   template<typename T>
   ComponentTraits make_component_traits() 
   {
     return ComponentTraits
     {
-      .size = sizeof(T),
-      .alignment = alignof(T),
-      .move_construct = [](void* dst, void* src) 
+      sizeof(T),
+      alignof(T),
+      
+      std::is_trivially_move_constructible_v<T> ? nullptr : [](void* dst, void* src) 
       {
-        new (dst) T(std::move(*static_cast<T*>(src)));
+          new (dst) T(std::move(*static_cast<T*>(src)));
       },
-      .copy_construct = [](void* dst, const void* src)
+      
+      std::is_trivially_copy_constructible_v<T> ? nullptr : [](void* dst, const void* src)
       {
-        new (dst) T(*static_cast<const T*>(src));
+          new (dst) T(*static_cast<const T*>(src));
       },
-      .destruct = [](void* ptr)
+      
+      std::is_trivially_destructible_v<T> ? nullptr : [](void* ptr)
       {
-        static_cast<T*>(ptr)->~T();
-      }
+          static_cast<T*>(ptr)->~T();
+      },
+
+      std::is_trivially_copyable_v<T>
     };
   }
 }
